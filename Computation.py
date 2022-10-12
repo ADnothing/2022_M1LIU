248b13f152155cf55273f2673a3b52b5b9c150e1
Adrien Anthore <75272960+ADnothing@users.noreply.github.com>
2022-10-12 17:53:10 +0200

    Add files via upload

--- Computation.py
+++ Computation.py
@@ -2,13 +2,13 @@ from main import *
 
 if __name__ == "__main__":
     
-    Vrad = input("Vrad km/s")
-    FHI = input("FHI Jy.km/s")
-    w20 = input("w20 km/s")
-    w50 = input("w50 km/s")
-    incl = input("incl rad")
-    logD25 = input("logD25 log(0.1 \")")
-    rms = input("rms mJy")
-    SNR = input("SNR")
+    Vrad =float(input("Vrad km/s : "))
+    FHI = float(input("FHI Jy.km/s : "))
+    w20 = float(input("w20 km/s : "))
+    w50 = float(input("w50 km/s : "))
+    incl = float(input("incl rad : "))
+    logD25 = float(input("logD25 log(0.1 \") : "))
+    rms = float(input("rms mJy : "))
+    SNR = float(input("SNR : "))
     
     printing(Vrad, FHI, w20, w50, incl, logD25, rms, SNR)
